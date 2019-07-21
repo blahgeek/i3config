cfcd841140058fa6c2e51aa099c7711bdd2d7286
BlahGeek <i@BlahGeek.com>
2019-07-21 22:23:51 +0800

    Move xcape to setup-keyboard

--- setup-keyboard.sh
+++ setup-keyboard.sh
@@ -4,5 +4,8 @@ sudo killall xkeysnail || true
 nohup sudo xkeysnail --watch ~/.config/i3/xkeysnail.config.py > /dev/null 2>&1 &
 sleep 2
 
+killall xcape
+xcape -e "Hyper_R=Menu"
+
 xmodmap ~/.Xmodmap
 xset r rate 200 25
