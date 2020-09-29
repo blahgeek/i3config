be031499c82e5b64d5a4d802a80e5fc111383e5b
BlahGeek <i@blahgeek.com>
2020-09-29 21:03:05 +0800

    Keyboard change. Ctrl moved near space

--- setup-keyboard.sh
+++ setup-keyboard.sh
@@ -1,11 +1,16 @@
 #!/bin/sh -ex
 
+# Use xkeysnail instead of xmodmap to remap keys because it works across keyboards (hotplug)
+# And it's much simpler
 sudo killall xkeysnail || true
 nohup sudo xkeysnail --watch ~/.config/i3/xkeysnail.config.py > /dev/null 2>&1 &
 sleep 3
 
-killall xcape || true
-xcape -e "Hyper_R=Menu"
+# Capslock is now Super_R.
+# Use it for IM switching only, remove it from mod4
 
-xmodmap ~/.Xmodmap
+# We need to add Super_R to another mod to make the removing working
+xmodmap -e "remove mod4 = Super_R"
+xmodmap -e "add mod3 = Super_R"
+
 xset r rate 200 25
--- xkeysnail.config.py
+++ xkeysnail.config.py
@@ -7,21 +7,13 @@ from xkeysnail.transform import (define_keymap, K, Key,
                                  define_modmap)
 
 
-# define_multipurpose_modmap({
-#     Key.CAPSLOCK: [Key.MENU, 199]
-# })
-
-TERM_APP_PATTERN = re.compile('bqterm|konsole', re.I)
-
-define_keymap(lambda s: not TERM_APP_PATTERN.search(s), {
-    K('Super-t'): K('Ctrl-t'),
-    K('Super-c'): K('Ctrl-c'),
-    K('Super-v'): K('Ctrl-v'),
-    K('Super-w'): K('Ctrl-w'),
-    K('Super-a'): K('Ctrl-a'),
-    K('Super-p'): K('Ctrl-p'),
-    K('Super-z'): K('Ctrl-z'),
-    K('Super-n'): K('Ctrl-n'),
-    K('Super-f'): K('Ctrl-f'),
-    K('Super-r'): K('Ctrl-r'),
+define_modmap({
+    Key.LEFT_CTRL: Key.LEFT_ALT,
+    Key.LEFT_META: Key.LEFT_CTRL,
+    Key.LEFT_ALT: Key.LEFT_META,
+    Key.CAPSLOCK: Key.RIGHT_META,
 })
+
+# define_multipurpose_modmap({
+#     Key.CAPSLOCK: [Key.RIGHT_META, Key.LEFT_META]
+# })
